eba67cdd2c47b2a786b073fd24be012316cb0e74
Miroshka <merey.sayrao@gmail.com>
2025-08-27 16:29:41 +1000

    fix vite.config.ts file

--- super-admin/vite.config.ts
+++ super-admin/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({ command }) => ({
+export default defineConfig({
 	plugins: [react()],
-	base: process.env.NODE_ENV === 'build' ? '/super-admin/' : '/',
+	base: process.env.NODE_ENV === 'production' ? '/super-admin/' : '/',
 	server: {
 		port: 3000,
 		fs: {
